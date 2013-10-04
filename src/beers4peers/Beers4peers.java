50466b8bd31c49009ba8f023ec858ae2043dc3a8
rewgoes <rewgoes@gmail.com>
2013-10-04 17:14:20 -0300

    Test

---
+++ src/beers4peers/Beers4peers.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beers4peers;
+
+/**
+ *
+ * @author rafael
+ */
+public class Beers4peers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
